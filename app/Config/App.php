a6af589dc7a36ee88b1985526d733efa73d4f25f
Manuel <mordonezsilva@gmail.com>
2023-08-23 22:16:25 -0600

    PRUEBA PARA PRUEBA2

--- app/Config/App.php
+++ app/Config/App.php
@@ -7,7 +7,7 @@ use CodeIgniter\Session\Handlers\FileHandler;
 
 class App extends BaseConfig
 {
-    /** antony
+    /** antony Y Manuel
      * --------------------------------------------------------------------------
      * Base Site URL
      * --------------------------------------------------------------------------
