3d467e9a7c92b6faba08cf858a8f29b7b192616c
bnflores92 <bernie.floress.bf@gmail.com>
2023-08-31 04:23:04 +0000

    si

--- app/Models/Model_Login.php
+++ app/Models/Model_Login.php
@@ -6,31 +6,24 @@ use CodeIgniter\Model;
 class Model_Login extends Model
 {
     
-    protected $table = 'per_personal'; // Cambia 'users' al nombre de tu tabla de usuarios
-    protected $primaryKey = 'per_id'; // Cambia 'id' al nombre de tu clave primaria
+    protected $table = 'per_personal'; // Cambia 'per_personal' al nombre de tu tabla
+    protected $primaryKey = 'per_id'; // Cambia 'per_id' al nombre de tu clave primaria
 
-    protected $allowedFields = ['per_correo', 'per_contrasena']; // Campos permitidos para insertar/actualizar
+    protected $allowedFields = ['per_nombre', 'per_apellido', 'per_correo', 'per_telefono', 'per_fecha_creacion', 'per_estado', 'per_nit', 'per_resguardo', 'per_acceso_sistema', 'per_contrasena', 'per_iddep', 'per_idcar']; // Campos permitidos para insertar/actualizar
 
-/*
-    function ValidarUsuario($per_correo, $per_contrasena){   
-        $db = \Config\Database::connect();
-    $session = \Config\Services::session();
-    $per_id = $session->get('per_id');
-    $builder = $db->table('per_personal');
+    public function getUserBy(string $column, string $value){
+        return $this->where($column, $value)->first();
+    }
 
 
-    $builder->select('*');
-    //$builder->join('mark_tbl', 'mark_tbl.sub_id = subject_tbl.sub_id');
-    $builder->where('per_correo', $per_correo);
-    $builder->where('per_contrasena', $per_contrasena);
-    $query = $builder->get();
-    return $query->getResult();
-        
-        //$Usuario = $this->db->query("SELECT * FROM per_personal WHERE per_correo = 'prueba@gmail.com' AND per_contrasena = '1234'");
-        //return $Usuario->getResult();
-     }
-  
-    */
+    public function actualizarUsuario($userData, $per_id){
+        return $this->update($per_id, $userData);
+    }
+
+    public function desactivarUsuario($userData, $per_id){
+        return $this->update($per_id, $userData);
+    }
+
 
     
 }
