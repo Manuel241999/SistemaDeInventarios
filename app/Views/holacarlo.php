592fae9f5b12331678bbdedd1c78786213270a2c
carlog9 <carlog0993@gmail.com>
2023-08-31 22:41:55 -0600

    Agregue pagina de prueba merge

---
+++ app/Views/holacarlo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--hash libreria de php para encriptar la contraseña; listado de los estado de compras 
+    y vista general 
+    Manuel: Vista admin, cargos, regiones, sburegiones, estado de transacciones de compras,
+    estado de activo y gestiones
+    home: se trabajara el listado general del ingreso de compras y vista de rechazo y aceptados.
+    Mostrar cantidad de datos. per_estado==Activo
+    registro de compra
+    Trim es para quitar espacios    
+    -->
+</body>
+</html>
