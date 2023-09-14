6d3a36febfd2d2d9ce51243aabd578439e0afc03
carlog9 <carlog0993@gmail.com>
2023-09-13 20:53:27 -0600

    Se borro rutas inecesarias

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -58,24 +58,7 @@ $routes->post('/DesactivarUsuarios', 'Admin::DesactivarUsuarios', ['as' => 'Desa
 //Compras
 $routes->get('/FormularioC', 'Home::FormularioC', ['as'=>'FormularioC']);
 $routes->get('/InicioCompras', 'Home::InicioCompras', ['as'=>'InicioCompras']);
-/*
-$route->get('/ActualizarCompras', 'compras::ActualizarCompras',['as'=>'ActualizarCompras']);
-$route->post('/ActualizarCompras', 'compras::ActualizarCompras');
 
-$route->get('/InsertarCompras', 'compras::RegistrodeCompras');
-$route->post('/InsertarCompras', 'compras::RegistrodeCompras');
-
-$route->get('/ListarCompras', 'compras::ListarComprar');
-$route->post('/ListarCompras', 'compras::ListarComprar');
-
-$route->get('/ActualizarTransaccion', 'est_transaccion::ActualizarestTransaccion');
-$route->post('/ActualizarTransaccion', 'est_transaccion::ActualizarestTransaccion');
-
-$route->get('/ListarTransCompras', 'est_transaccion::ListarEstTransaccion');
-$route->post('/ListarTransCompras', 'est_transaccion::ListarEstTransaccion');
-
-$route->get('/InsertarCompras', 'est_transaccion::RegistroestestTransaccion');
-$route->post('/InsertrCompras', 'est_transaccion::RegistroestTransaccion');*/
 
 /*
  * --------------------------------------------------------------------
